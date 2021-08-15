f168f7e88aba615fafdcc95e492e510d310c9c24
MohammadAltamimi98 <m98altamimi@gmail.com>
2021-08-15 21:44:19 +0300

    commit

---
+++ 401-challenges/Hash-repeatedWord/README.md
@@ -0,0 +1,17 @@
+## Code Challenge: Class 31
+
+- Find the first repeated word in a book.
+<br/>
+<br/>
+
+## Feature Tasks
+- Write a function called repeated word that finds the first word to occur more than once in a string
+- Arguments: string
+- Return: array
+<br/>
+<br/>
+
+
+## whiteBoard
+
+![](./repeated.png)
--- 401-challenges/Hash-repeatedWord/index.js
+++ 401-challenges/Hash-repeatedWord/index.js
@@ -4,21 +4,15 @@ function repeatedWords(string) {
     console.log(split.length);
     console.log(split);
     let containerArray = [];
-
     let words = [];
-
     for (i = 0; i < split.length; i++) {
       let formatWord = split[i].toLowerCase();
-      // console.log(split[i]);
       let container = words.includes(formatWord);
-      // console.log(container);
-
       if (container) {
         containerArray.push(formatWord);
       }
       words.push(formatWord);
     }
-
     if (containerArray.length === 0) return 'no repeatitions';
     return containerArray;
 
