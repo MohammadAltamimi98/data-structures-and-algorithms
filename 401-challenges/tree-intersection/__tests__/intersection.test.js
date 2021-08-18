d813029366e3d5ea66e040b4930bbff348fcfdec
MohammadAltamimi98 <m98altamimi@gmail.com>
2021-08-18 20:32:19 +0300

    commit

--- 401-challenges/tree-intersection/__tests__/intersection.test.js
+++ 401-challenges/tree-intersection/__tests__/intersection.test.js
@@ -35,11 +35,36 @@ describe('testing the intersection', () => {
 
 
 
-  test('should console tree', () => {
-    expect(tree.treeIntersection(tree.root, tree1.root)).isArray[1, 3, 2, 5, 4];
+  test('should return array of intersections', () => {
+    expect(tree.treeIntersection(tree.root, tree1.root)).toStrictEqual([1, 3, 6, 2, 5, 4]);
+  })
 
+  test('should throw an error if one of the roots were not provided ', () => {
+    expect(() => {
+      tree.treeIntersection(tree.root);
+    }).toThrow('there is no root to comapare');
+  })
+
+  test('should return partial intersection when only a part intersects', () => {
+    const a = new Node(1);
+    const b = new Node(2);
+    const c = new Node(3);
+    const d = new Node(4);
+    const e = new Node(5);
+    // const f = new Node(6);
+
+    a.right = b;
+    a.left = c;
+    b.right = d;
+    b.left = e;
+    // c.right = f;
+
+
+    let tree1 = new BinaryTree(a);
+    expect(tree.treeIntersection(tree.root, tree1.root)).toStrictEqual([1, 3, 2, 5, 4]);
 
   })
+
 
 })
 
